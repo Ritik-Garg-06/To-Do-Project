6b2aa2daddc084414d93305903d276178cf430f1
Ritik-Garg-06 <102740567+Ritik-Garg-06@users.noreply.github.com>
2023-10-13 00:48:05 +0530

    Written Html and CSS Further and Removed Bug of Size

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+let plus_sign = document.querySelector(".left-lower-contents");
+let main_container = document.querySelector(".blur");
+
+function promptadd()
+{
+    console.log("Clicked");
+    main_container.classList.add('active');
+}
+
+plus_sign.addEventListener("click",promptadd);
