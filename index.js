e2dc5430033c86417ea49b971bc78ac646be192e
Ritik-Garg-06 <102740567+Ritik-Garg-06@users.noreply.github.com>
2023-10-13 20:23:50 +0530

    Added the first Prompt to add a note

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 let plus_sign = document.querySelector(".left-lower-contents");
-let main_container = document.querySelector(".blur");
+let main_container = document.querySelector(".container");
+let prompt = document.querySelector('.Insert-Prompt');
+let blur = document.querySelector('.blur');
+let close = document.querySelector('.Close-Prompt');
+
 
 function promptadd()
 {
-    console.log("Clicked");
-    main_container.classList.add('active');
+    blur.classList.remove('hidden');   
+    prompt.classList.remove('hidden');
+    prompt.classList.add('visible');
+}
+
+function ClosePrompt()
+{
+    blur.classList.add('hidden');   
+    prompt.classList.add('hidden');
+    prompt.classList.remove('visible');
 }
 
 plus_sign.addEventListener("click",promptadd);
+close.addEventListener("click",ClosePrompt);
