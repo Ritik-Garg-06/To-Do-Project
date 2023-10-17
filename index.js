a15b4ae21b7d0c7d3583ceabc730e3f19d19f9a2
Ritik-Garg-06 <102740567+Ritik-Garg-06@users.noreply.github.com>
2023-10-17 21:35:20 +0530

    Added the json object

--- index.js
+++ index.js
@@ -19,9 +19,19 @@ let To_Do_container = document.querySelector(".Prompt-right-container-To-Do");
 const rightContainer = document.querySelector(".right-container");
 const ulElements = rightContainer.querySelectorAll("ul");
 let InputBoxTitle = document.querySelector("#Create-Note-Title");
+let InputBoxTitleForEdit = document.querySelector("#Create-Note-Title-forEdit");
 let InputBoxDesc = document.querySelector("#Create-Note-Description");
+let InputBoxDescForEdit = document.querySelector("#Create-Note-Description-forEdit");
+let Update_Prompt = document.querySelector(".edit-container");
+let priority = document.querySelectorAll('[name="options"]');
+let priorityForEdit = document.querySelectorAll('[name="options-forEdit"]');
+let close_edit_prompt = document.querySelector(".close-edit");
+let updateButton = document.querySelector(".update-button");
+let FormDueDate = document.querySelector("#Create-Note-Due-Date");
+let FormDueDateForEdit = document.querySelector("#Create-Note-Due-Date-forEdit");
 let curr_tab = HomeTab;
 let curr_content = HomeTab_Content;
+let taskIndex = 0;
 
 function switchTab(contentList) {
   ulElements.forEach((section) => {
@@ -32,34 +42,125 @@ function switchTab(contentList) {
   contentList.classList.remove("hidden");
 }
 
+let tasks = [];
+
 function addToDo() {
   if (InputBoxTitle.value === "") {
     alert("You must write something!");
   } else {
-    let li = document.createElement("li");
-    li.innerHTML = InputBoxTitle.value;
+    // Get the task title and description from the input fields
+    const taskText = InputBoxTitle.value;
+    const taskDescription = InputBoxDesc.value;
 
-    let span1 = document.createElement("span");
-    span1.innerHTML = '<i class="fas fa-pencil-alt"></i>';
-    span1.classList.add("Cross", "Update-Data");
-    li.appendChild(span1);
-    
-    let span2 = document.createElement("span");
+    // Set default values for priority and due date
+    let selectedValue = null;
+
+    // Loop through the radio buttons to find the selected one
+    for (const radioButton of priority) {
+      if (radioButton.checked) {
+        selectedValue = radioButton.value;
+        break; // Exit the loop once a selected radio button is found
+      }
+    }// You can customize this later
+    console.log(selectedValue);
+    const taskDueDate = FormDueDate.value; // Get the current date and time as a default due date
+
+    // Create a JSON object to represent the new task
+    const newTask = {
+      title: taskText, // Task title
+      description: taskDescription, // Task description
+      priority: selectedValue, // Task priority (set to default)
+      dueDate: taskDueDate, // Task due date (set to the current date and time)
+      category: curr_tab.classList.contains("Home-Name")
+        ? "Home"
+        : curr_tab.classList.contains("Today-task-Name")
+        ? "Today"
+        : curr_tab.classList.contains("Weekly-Tasks-Name")
+        ? "Week"
+        : "Other",
+    };
+
+    // Add the new task object to the existing tasks array
+    tasks.push(newTask);
+
+    // Get the index of the newly added task
+    taskIndex = tasks.length - 1;
+
+    // Create an HTML list item for the new task
+    const li = document.createElement("li");
+    li.textContent = newTask.title; // Display the task title in the list item
+
+    // Store the task's index in the data-index attribute of the list item
+    li.dataset.index = taskIndex;
+
+    // Create an "Edit" button for the task
+    const span1 = document.createElement("span");
+    span1.innerHTML = '<img src="Images/edit.png" class="Update-Data"/>';
+
+    // Create a "Delete" button for the task
+    const span2 = document.createElement("span");
     span2.innerText = "\u00d7";
     span2.classList.add("Cross");
+
+    // Add event listeners for the "Edit" and "Delete" buttons
+    // span1.addEventListener("click", () => openEdit(taskIndex));
+    // span2.addEventListener("click", () => deleteTask(taskIndex));
+
+    // Append the "Edit" and "Delete" buttons to the list item
+    li.appendChild(span1);
     li.appendChild(span2);
 
-    if (curr_tab == HomeTab) {
-      HomeTab_Content.appendChild(li);
-    } else if (curr_tab == TodayTab) {
-      TodayTab_Content.appendChild(li);
-    } else if (curr_tab == WeekTab) {
-      WeekTab_Content.appendChild(li);
-    }
+    // Append the new task item to the current content area (tab's task list)
+    curr_content.appendChild(li);
   }
+
+  // Clear the input fields and close the task creation prompt
   InputBoxTitle.value = "";
   InputBoxDesc.value = "";
   ClosePrompt();
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  renderTasks(
+    curr_tab.classList.contains("Home-Name")
+      ? "Home"
+      : curr_tab.classList.contains("Today-task-Name")
+      ? "Today"
+      : "Week"
+  );
+}
+
+function renderTasks(category) {
+  const taskList = curr_content;
+  taskList.innerHTML = ""; // Clear the task list
+
+  tasks
+    .filter((task) => task.category === category)
+    .forEach((task, index) => {
+      const li = document.createElement("li");
+      li.textContent = task.title;
+      li.dataset.index = index;
+
+      const span1 = document.createElement("span");
+      span1.innerHTML = '<img src="Images/edit.png" class="Update-Data"/>';
+
+      const span2 = document.createElement("span");
+      span2.innerText = "\u00d7";
+      span2.classList.add("Cross");
+
+      // span1.addEventListener("click", (e) =>
+      //   openEdit(e.target.parentElement.dataset.index)
+      // );
+      // span2.addEventListener("click", (e) =>
+      //   deleteTask(e.target.parentElement.dataset.index)
+      // );
+
+      li.appendChild(span1);
+      li.appendChild(span2);
+
+      taskList.appendChild(li);
+    });
 }
 
 function promptadd() {
@@ -93,7 +194,68 @@ function handleItemClick(e) {
     e.target.parentElement.remove();
   }
 }
+function openEdit(taskIndex) {
+  // Get the task object from the tasks array using the provided taskIndex
+  console.log(taskIndex);
+  const task = tasks[taskIndex];
+  console.log(task);
 
+  // Populate the input fields in the edit prompt with task information
+  InputBoxTitleForEdit.value = task.title;
+  InputBoxDescForEdit.value = task.description;
+  FormDueDateForEdit.value = task.dueDate;
+  priorityForEdit.value = task.priority;
+
+  // Store the task index for later use when saving the edited task
+  taskTextForEditing = taskIndex;
+
+  // Show the edit prompt by removing the "hidden" class
+  blur.classList.remove("hidden");
+  Update_Prompt.classList.remove("hidden");
+
+  // Add an event listener to the "Update" button in the edit prompt
+  updateButton.addEventListener("click",function(){
+    editToDo(taskTextForEditing)
+  });
+}
+
+function editToDo(taskTextForEditing) {
+  // Get the modified title and description from the input fields in the edit prompt
+  const modifiedTitle = InputBoxTitleForEdit.value;
+  const modifiedDescription = InputBoxDescForEdit.value;
+  const modifiedDate = FormDueDateForEdit.value;
+  const modifiedPriority = null;
+
+    // Loop through the radio buttons to find the selected one
+    for (const radioButton of priority) {
+      if (radioButton.checked) {
+        selectedValue = radioButton.value;
+        break; // Exit the loop once a selected radio button is found
+      }
+    }
+
+  // Get the index of the task you want to edit (this should be stored somewhere, like taskTextForEditing)
+  const EditIndex = taskTextForEditing;
+
+  // Update the task with the modified title and description
+  tasks[EditIndex].title = modifiedTitle;
+  tasks[EditIndex].description = modifiedDescription;
+  tasks[EditIndex].dueDate = modifiedDate;
+  tasks[EditIndex].priority = modifiedPriority;
+  console.log(modifiedPriority);
+
+  renderTasks(curr_tab.classList.contains("Home-Name") ? "Home" : "Today");
+
+  CloseEdit();
+}
+
+
+
+function CloseEdit() {
+  blur.classList.add("hidden");
+  Update_Prompt.classList.add("hidden");
+}
+close_edit_prompt.addEventListener("click", CloseEdit);
 plus_sign.addEventListener("click", promptadd);
 close.addEventListener("click", ClosePrompt);
 Prompt_add_To_do.addEventListener("click", openToDo);
@@ -129,15 +291,18 @@ NotesTab.addEventListener("click", () => {
 });
 
 function attachClickListener(content) {
-  content.addEventListener(
-    "click",
-    function (e) {
+  content.addEventListener("click", function (e) {
+    const listItem = e.target.closest("li");
+    if (listItem) {
       if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-      } else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
+        listItem.classList.toggle("checked");
+      } else if (e.target.classList.contains("Update-Data")) {
+        console.log(listItem.dataset.index);
+        openEdit(listItem.dataset.index);
+      } else if (e.target.classList.contains("Cross")) {
+        deleteTask(listItem.dataset.index);
       }
-    },
-    false
-  );
+    }
+  });
 }
+
